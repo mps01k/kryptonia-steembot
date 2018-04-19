1a9ab051839e07b1537bd932c05e33d22d290d9b
NineCloud <gabrielarlo11@gmail.com>
2018-04-19 16:15:49 +0800

    multi voter init

--- voter.js
+++ voter.js
@@ -10,7 +10,9 @@ var steem = require('steem');
     var host = 'http://localhost:8000';
     var email = "gabrielarlo11@gmail.com";
     var password = "password";
-    steem.api.setOptions({ url: 'https://api.steemit.com' });
+    steem.api.setOptions({
+        url: 'https://api.steemit.com'
+    });
     steem.config.set('address_prefix', 'STM');
 
     w.last_task_id = null;
@@ -112,17 +114,38 @@ var steem = require('steem');
 
     w.update_candidate_status = function (id, voter, status = 1) {
         console.log('Passed Item ID: ' + id);
-        var sql = `UPDATE steem_vote_lists SET status = ${status}, voter = '${voter.username}', weight = '${voter.weight}' WHERE id = ${id}`;
-        db_con.query(sql, function (err, result, fields) {
-            if (err) {
-                throw err;
-            }
-            if (status == 1) {
-                console.log('Steem Blog Voted');
-            } else {
-                console.log('Skipped for voting');
-            }
-        });
+        if (status == 1) {
+            var sql = `SELECT id, counter FROM steem_vote_lists WHERE id = ${id}`;
+            db_con.query(sql, function (err, result, fields) {
+                if (err) {
+                    throw err;
+                }
+                var new_counter = result[0].counter + 1;
+                var sql = `UPDATE steem_vote_lists SET status = ${status}, voter = '${voter.username}', weight = '${voter.weight}', counter = ${new_counter} WHERE id = ${id}`;
+                db_con.query(sql, function (err, result, fields) {
+                    if (err) {
+                        throw err;
+                    }
+                    if (status == 1) {
+                        console.log('Steem Blog Voted');
+                    } else {
+                        console.log('Skipped for voting');
+                    }
+                });
+            });
+        } else {
+            var sql = `UPDATE steem_vote_lists SET status = ${status}, voter = '${voter.username}', weight = '${voter.weight}' WHERE id = ${id}`;
+            db_con.query(sql, function (err, result, fields) {
+                if (err) {
+                    throw err;
+                }
+                if (status == 1) {
+                    console.log('Steem Blog Voted');
+                } else {
+                    console.log('Skipped for voting');
+                }
+            });
+        }
     };
 
     w.start_voting = function (voter) {
@@ -132,33 +155,159 @@ var steem = require('steem');
                     console.log("Currently on voting que: TaskID-", result.data.task_id);
 
                     var permalink = result.data.permalink;
-                    permalink = permalink.substring(permalink.indexOf('@') + 1);
-                    author = result.data.author;
-                    permalink = permalink.substring(author.length + 1);
-
-                    var item = {
-                        username: voter.username,
-                        password: voter.password,
-                        author: author,
-                        permalink: permalink,
-                        weight: voter.weight,
-                        item_id: result.data.id,
-                    };
-                    w.vote_it(item, function (response) {
-                        if (response.status == 200) {
-                            w.update_candidate_status(item.item_id, voter, 1);
-                            console.info(response.msg);
-                        } else {
-                            w.update_candidate_status(item.item_id, voter, 2);
-                            console.error(response.msg);
+                    if (permalink.indexOf('@') >= 0) {
+                        permalink = permalink.substring(permalink.indexOf('@') + 1);
+                        author = result.data.author;
+                        permalink = permalink.substring(author.length + 1);
+                        if (permalink.indexOf('#') >= 0) {
+                            permalink = permalink.substring(0, permalink.indexOf('#'));
                         }
-                    });
+
+                        var weight = voter.weight;
+                        steem.api.getContent(author, permalink, function (err, res) {
+                            if (err != null) {
+                                console.error("Error fetching Blog Post");
+                            } else {
+                                $meta = JSON.parse(res.json_metadata).tags;
+                                $tags = voter.tags;
+                                var found = 0;
+                                $meta.forEach(tag => {
+                                    if ($tags.indexOf(tag) >= 0) {
+                                        found = 1;
+                                    }
+                                });
+                                if (found == 0) {
+                                    weight = voter.min_weight;
+                                } else {
+                                    weight = voter.weight;
+                                }
+                                var item = {
+                                    username: voter.username,
+                                    password: voter.password,
+                                    author: author,
+                                    permalink: permalink,
+                                    weight: weight,
+                                    item_id: result.data.id,
+                                };
+                                w.vote_it(item, function (response) {
+                                    if (response.status == 200) {
+                                        w.update_candidate_status(result.data.id, item, 1);
+                                        console.info(response.msg);
+                                    } else {
+                                        var item2 = {
+                                            username: '',
+                                            weight: 0
+                                        };
+                                        w.update_candidate_status(result.data.id, item2, 2);
+                                        console.error(response.msg);
+                                    }
+                                });
+                            }
+                        });
+                    } else {
+                        var item2 = {
+                            username: '',
+                            weight: 0
+                        };
+                        w.update_candidate_status(result.data.id, item2, 2);
+                        console.error('No Author');
+                    }
                 } else {
                     console.error(result.msg);
                 }
             });
         } else {
+            axios({
+                    url: host + '/api/get-list-of-voters',
+                    method: 'POST',
+                    responseType: 'json',
+                    data: {
+                        email: email,
+                        password: password
+                    }
+                })
+                .then(function (rem_res) {
+                    if (rem_res.data.status == 200) {
+                        $voters = rem_res.data.data;
+                        $voters.forEach($voter => {
+                            setTimeout(function () {
+                                w.get_vote_candidate(function (result) {
+                                    if (result.status == 200) {
+                                        console.log("Currently on voting que: TaskID-", result.data.task_id);
 
+                                        var permalink = result.data.permalink;
+                                        if (permalink.indexOf('@') >= 0) {
+                                            permalink = permalink.substring(permalink.indexOf('@') + 1);
+                                            author = result.data.author;
+                                            permalink = permalink.substring(author.length + 1);
+                                            if (permalink.indexOf('#') >= 0) {
+                                                permalink = permalink.substring(0, permalink.indexOf('#'));
+                                            }
+
+                                            var weight = voter.weight;
+                                            steem.api.getContent(author, permalink, function (err, res) {
+                                                if (err != null) {
+                                                    console.error("Error fetching Blog Post");
+                                                } else {
+                                                    $meta = JSON.parse(res.json_metadata).tags;
+                                                    $tags = voter.tags;
+                                                    var found = 0;
+                                                    $meta.forEach(tag => {
+                                                        if ($tags.indexOf(tag) >= 0) {
+                                                            found = 1;
+                                                        }
+                                                    });
+                                                    if (found == 0) {
+                                                        weight = voter.min_weight;
+                                                    } else {
+                                                        weight = voter.weight;
+                                                    }
+                                                    var item = {
+                                                        username: $voter.username,
+                                                        password: $voter.password,
+                                                        author: author,
+                                                        permalink: permalink,
+                                                        weight: weight,
+                                                        item_id: result.data.id,
+                                                    };
+                                                    w.vote_it(item, function (response) {
+                                                        if (response.status == 200) {
+                                                            var item2 = {
+                                                                username: 'multi',
+                                                                weight: item.weight
+                                                            };
+                                                            w.update_candidate_status(result.data.id, item2, 1);
+                                                            console.info(response.msg);
+                                                        } else {
+                                                            var item2 = {
+                                                                username: '',
+                                                                weight: 0
+                                                            };
+                                                            w.update_candidate_status(result.data.id, item2, 2);
+                                                            console.error(response.msg);
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        } else {
+                                            var item2 = {
+                                                username: '',
+                                                weight: 0
+                                            };
+                                            w.update_candidate_status(result.data.id, item2, 2);
+                                            console.error('No Author');
+                                        }
+                                    } else {
+                                        console.error(result.msg);
+                                    }
+                                });
+                            }, 15000);
+                        });
+                    }
+                })
+                .catch(function (err) {
+                    // console.error(err);
+                });
         }
     };
 
